40a2de78c42148b2d50f9e2b0c0d89d729b9ba84
HHamlet <hh.harutyunyan@mail.ru>
2025-01-25 22:38:24 +0400

    some modification

--- db/db.py
+++ db/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from db.core.config import CONNECTION_STRING
 
-engine = create_async_engine(CONNECTION_STRING, echo=True)
+engine = create_async_engine(CONNECTION_STRING, echo=True, pool_size=10, max_overflow=20)
 async_session = async_sessionmaker(engine, expire_on_commit=False)
